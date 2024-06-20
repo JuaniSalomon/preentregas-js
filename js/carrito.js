aacb54b73324a6c688ceae0d80ecc87a78d26178
juani salomon <jigsalomon98@gmail.com>
2024-06-20 00:04:39 -0300

    agregue operadores ternarios, storage y JSON

--- js/carrito.js
+++ js/carrito.js
@@ -1,43 +1,65 @@
 const availableProducts = [
-  { id: 1, nombre: "Producto 1", precio: 10 },
-  { id: 2, nombre: "Producto 2", precio: 20 },
-  { id: 3, nombre: "Producto 3", precio: 30 },
+  { id: 1, name: "Producto 1", price: 10 },
+  { id: 2, name: "Producto 2", price: 20 },
+  { id: 3, name: "Producto 3", price: 30 },
 ];
-
-let carrito = [];
 
 let total = 0;
 
+// function to get cart from local storage and parse it"
+function getCart() {
+  const cartStringify = localStorage.getItem("cart");
+  const cart = cartStringify ? JSON.parse(cartStringify) : [];
+  return cart;
+}
+
 function addProduct(id) {
   const product = availableProducts.find((prod) => prod.id === id);
+  const cart = getCart();
   if (product) {
-    carrito.push(product);
-    total += product.precio;
-    actualizarCarrito();
+    cart.push(product);
+    // stringify array and save it in local storage
+    const cartStringify = JSON.stringify(cart);
+    localStorage.setItem("cart", cartStringify);
+    total += product.price;
+
+    updateCart();
   }
 }
 
-function actualizarCarrito() {
-  const carritoElement = document.getElementById("carrito");
+function updateCart() {
+  const cartElement = document.getElementById("cart");
   const totalElement = document.getElementById("total");
+  // get cart from local storage and parse it
+  const cart = getCart();
 
-  carritoElement.innerHTML = "";
+  cartElement.innerHTML = "";
 
-  carrito.forEach((product) => {
+  cart.forEach((product) => {
+    const { name, price } = product;
     const productElement = document.createElement("div");
-    productElement.textContent = `${product.nombre} - $${product.precio}`;
-    carritoElement.appendChild(productElement);
+    productElement.textContent = `${name} - $${price}`;
+    cartElement.appendChild(productElement);
   });
 
   totalElement.textContent = `Total: $${total}`;
 }
 
+// save cart products when refresh the page
+updateCart();
+
 document
-  .getElementById("producto1")
+  .getElementById("product1")
   .addEventListener("click", () => addProduct(1));
 document
-  .getElementById("producto2")
+  .getElementById("product2")
   .addEventListener("click", () => addProduct(2));
 document
-  .getElementById("producto3")
+  .getElementById("product3")
   .addEventListener("click", () => addProduct(3));
+
+document.getElementById("buy").addEventListener("click", () => {
+  localStorage.removeItem("cart");
+  total = 0;
+  updateCart();
+});
